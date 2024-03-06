c12f08431dd758ce63fc40cfd740b2aa82d4ef4e
Luiz Magrini <a40031@esas.pt>
2024-03-06 10:53:55 +0000

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +21,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
