06bd23e471d0bd2e2bc7beb4e691dd60c1941cd2
Luiz Magrini <a40031@esas.pt>
2024-04-01 12:34:59 +0100

    Section 8

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,9 +11,8 @@ namespace API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
-        {
-        }
-        public DbSet<AppUser> Users {get;set;}
+        { }
+        public DbSet<AppUser> Users { get; set; }
 
     }
 }
